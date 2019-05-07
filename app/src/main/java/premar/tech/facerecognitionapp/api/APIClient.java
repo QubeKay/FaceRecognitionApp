67dd970169743e269f740ae6512174885a6a79c5
Kelvin Chot <kinyunye@gmail.com>
2019-05-07 05:43:45 +0300

    Working copy before starting on registration UI

--- app/src/main/java/premar/tech/facerecognitionapp/api/APIClient.java
+++ app/src/main/java/premar/tech/facerecognitionapp/api/APIClient.java
@@ -1,5 +1,7 @@
 package premar.tech.facerecognitionapp.api;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -13,7 +15,11 @@ public class APIClient {
 
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .build();
 
 
         retrofit = new Retrofit.Builder()
